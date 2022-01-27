a39bc708e3752ff8d0cb6a3fd18b13d727a11330
Debabrata47 <78600746+Debabrata47@users.noreply.github.com>
2022-01-27 15:12:30 +0530

    updated

--- Gui.py
+++ Gui.py
@@ -13,39 +13,46 @@ window.geometry('1000x900')
 window.configure(bg='gray')
 l1.grid(column=0, row=0)
 
-
-
-
 # Importing Images
-slot_empty = Image.open('green.png')
-slot_empty = slot_empty.resize((100, 130), Image.ANTIALIAS)
+slot_empty = Image.open('SE_1.jpg')
+slot_empty = slot_empty.resize((80, 90), Image.ANTIALIAS)
 my_img = ImageTk.PhotoImage(slot_empty)
-slot_filled = Image.open('red.png')
-slot_filled = slot_filled.resize((100, 130), Image.ANTIALIAS)
+slot_filled = Image.open('SF_1.png')
+slot_filled = slot_filled.resize((80, 90), Image.ANTIALIAS)
 my_img2 = ImageTk.PhotoImage(slot_filled)
-
 colour="yellow" 
-# Creating 69 consecutive slots
-##
+#
 count=0
 bt=[]
+spacesEmpty={1,6,25,63}
+
 for i in range(2,10):
-     if i==4 :
-          l3 = tkinter.Label(window, bg="gray", text="Road")
-          l3.grid(row=4) 
-          continue;
-     if i==7 :
-          l3 = tkinter.Label(window, bg="gray", text="Road")
-          l3.grid(row=7)  
-          continue; 
+      if i==4 :
+           l3 = tkinter.Label(window, bg="gray", text="Road")
+           l3.grid(row=4) 
+           continue;
+      if i==7 :
+           l3 = tkinter.Label(window, bg="gray", text="Road")
+           l3.grid(row=7)  
+           continue; 
+            
+
+      for j in range(1,13):
+          temp=count +1
+          if temp in spacesEmpty :
+               bt.append(tkinter.Button(window, text="Slot "+str(count+1), image=my_img, compound='top', borderwidth=3, relief="solid"))
+               bt[count].grid(column=j, row=i)  
+               count=count+1
+          else :
+               bt.append(tkinter.Button(window, text="Slot "+str(count+1), image=my_img2, compound='top', borderwidth=3, relief="solid"))
+               bt[count].grid(column=j, row=i)  
+               count=count+1
           
 
-     for j in range(1,13):
-          bt.append(tkinter.Button(window, text="Slot "+str(count+1), image=my_img, compound='top', borderwidth=3, relief="solid"))
-          bt[count].grid(column=j, row=i)  
-          count=count+1
-##
 
-# Main loop to keep the window open as long as user wants
+
+
+
+
 window.mainloop()                                                                                                                                                                
 
